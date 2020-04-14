f3460962bbad3c3f61b39749be976af871df14c3
[Oluwaseun Aderinlokun] <[aderinlokunoluwaseun@gmail.com]>
2020-04-14 11:33:48 +0000

    Separated xml and json responses

--- api/routes/estimate.js
+++ api/routes/estimate.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const xml = require('xml2js')
+
 const covid19ImpactEstimator = require('../scripts/estimator')
 
 const router = express.Router()
@@ -19,6 +21,12 @@ const dataTest = (data) => {
     }
 }
 
+const builder = new xml.Builder({
+    renderOpts: {
+        pretty: false
+    }
+})
+
 router.post('/', (req,res,next) => {
     if(dataTest(req.body)){
         res.status(200).json({
@@ -28,6 +36,28 @@ router.post('/', (req,res,next) => {
         res.status(404).json({
             message: "Incorrect data format was passed."
         })
+    }   
+})
+
+router.post('/json', (req,res,next) => {
+    if(dataTest(req.body)){
+        res.status(200).json({
+            result: covid19ImpactEstimator(req.body)
+        })
+    } else{
+        res.status(404).json({
+            message: "Incorrect data format was passed."
+        })
+    }   
+})
+
+router.post('/xml', (req,res,next) => {
+    if(dataTest(req.body)){
+        res.status(200).send(builder.buildObject( covid19ImpactEstimator(req.body) ))
+    } else{
+        res.status(404).send(builder.buildObject({ 
+            message: "Incorrect data format was passed."
+         }))
     }   
 })
 
