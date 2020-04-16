eb7a441f9a3858dfdfc1ead73374cba00e2669bc
[Oluwaseun Aderinlokun] <[aderinlokunoluwaseun@gmail.com]>
2020-04-16 04:22:16 +0000

    Made response time minimum of 2 digits

--- api/middleware/logger.js
+++ api/middleware/logger.js
@@ -3,7 +3,7 @@ const { createLogger, transports, format } = require('winston');
 const logger = createLogger({
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
-    format.printf((info) => `${info.message}`)
+    format.printf((info) => `${info.message.split('\t\t')[3].indexOf('ms') === 1 ? `${info.message.split('\t\t')[0]}\t\t${info.message.split('\t\t')[1]}\t\t${info.message.split('\t\t')[2]}\t\t0${info.message.split('\t\t')[3]}` : info.message}`)
   ),
   transports: [
     new transports.File({
